624146cbbb178bf4c430233dc8e453548fcda90d
atharizaki <atharizaki4@gmail.com>
2022-11-13 10:55:14 +0700

    first commit

---
+++ src/test/java/pws/c/ProjectIDcard/ProjectIDcardApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.c.ProjectIDcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIDcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
