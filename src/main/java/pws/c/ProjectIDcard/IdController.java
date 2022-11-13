e17847676c5eef9b71607a0a18a8ceeb10ea0f17
atharizaki <atharizaki4@gmail.com>
2022-11-13 11:27:38 +0700

    Menambahkan Controller

---
+++ src/main/java/pws/c/ProjectIDcard/IdController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pws.c.ProjectIDcard;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author athar
+ */
+@Controller
+public class IdController {
+    @RequestMapping("/ProjectCard")
+    public String getData(@RequestParam("Name") String Nama,
+                          @RequestParam("Tanggal")@DateTimeFormat(pattern ="yyyy-dd-MMMM") Date date,
+                          @RequestParam("Gambar") MultipartFile file,
+                          Model model) throws IOException{
+        
+        SimpleDateFormat idc = new SimpleDateFormat("EEEE, dd MMMM yyyy");
+        String tanggal = idc.format(date);
+        
+        String blob = Base64.encodeBase64String(file.getBytes());
+        String gambar = "data:image/jpeg;base64,".concat(blob);
+        
+        model.addAttribute("Nm", Nama);
+        model.addAttribute("tgl", tanggal);
+        model.addAttribute("gmbr", gambar);
+                
+        return "card";
+    }
+            
+}
